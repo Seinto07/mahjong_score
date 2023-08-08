5d23b956711ee391632be3a0203a2a8767f9aff6
Seiya Takahashi <cmt14075@ict.nitech.ac.jp>
2023-08-08 21:33:53 +0900

    PlayerクラスとMarkCalculatorクラスの作成

--- Player.java
+++ Player.java
@@ -1,11 +1,36 @@
 public class Player {
-    String name;
-    int haveScore;
-    char wind;
+    private String name;
+    private int haveScore;
+    private char wind;
 
     public Player(String name, int haveScore, char wind) {
         this.name = name;
         this.haveScore = haveScore;
         this.wind = wind;
     }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public int getHaveScore() {
+        return this.haveScore;
+    }
+
+    public char getWind() {
+        return this.wind;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setHaveScore(int score) {
+        this.haveScore = score;
+    }
+
+    public void setWind(char wind) {
+        this.wind = wind;
+    }
+
 }
